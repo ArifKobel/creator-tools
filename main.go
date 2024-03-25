5bcba6da01fd48cc3a0f70e35ebd54322c863a39
Arif K <102538661+ArifKobel@users.noreply.github.com>
2024-03-25 12:26:45 +0100

    (feat): add sample to main.go

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/ArifKobel/creator-tools/services"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Creator Tools")
+	godotenv.Load()
+	inputFile := "input.mp4"
+	taskId, err := services.RandStringRunes(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// create a folder for the task
+	err = os.Mkdir("tasks/"+taskId, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	services.ConvertToWAV(inputFile, "tasks/"+taskId+"/input.wav")
+	subtitles, err := services.GenerateSubTitles("tasks/" + taskId + "/input.wav")
+	if err != nil {
+		fmt.Println(err)
+	}
+	services.RemoveFile("tasks/" + taskId + "/input.wav")
+	services.AddSubtitlesToVideo(inputFile, subtitles, "tasks/"+taskId+"/output.mp4", taskId)
+
 }
